660a1387e1005b3907a4382457121e37d9356835
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-07-26 19:16:02 -0400

    added the rest of the list

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -23,44 +23,34 @@ const Portfolio = () => (
         <div className="item">
           <a href="https://hospitality-1.herokuapp.com/">
           <img src="/static/hosppre.png"  />
-          </a>
           <h2>Hospitality</h2>
-          <p>Hospitality is a healthcare portal for medical professionals to add, store and update patient records in a simple way. It is also tracks current treats such as the Corona virus.</p>
+          </a>
+          <p id="p1">Hospitality is a healthcare portal for medical professionals to add, store and update patient records in a simple way. It is also tracks current treats such as the Corona virus.</p>
+          <p id="p2">Tools: HTML, JavaScript, CSS, Nodejs, Express, MySQL</p>
         </div>
         <div className="item">
           <a href="https://makiwumi.github.io/Weather-Dashboard/">
           <img src="/static/weatherpre.png"  />
-          </a>
           <h2>Weather Dashboard</h2>
-          <p>A dashboard that displays the weather from an API using HTML, CSS, JavaScript and Bootstrap.</p>
+          </a>
+          <p id="p1">A dashboard that displays the weather in each city searched for.</p>
+          <p id="p2">Tools: HTML, CSS, JavaScript, Bootstrap and OpenWeather API</p>
         </div>
         <div className="item">
           <a href="https://burger-galore.herokuapp.com/">
           <img src="/static/Eatpre.png"  />
-          </a>
           <h2>Eat-Da-Burger</h2>
-          <p>An app that allows you to add your favorite burgers and devour them!</p>
+          </a>
+          <p id="p1">An app that allows you to add your favorite burgers and devour them!</p>
+          <p id="p2">Tools: Handlebars, CSS, Nodejs, Express, MySQL</p>
         </div>
         <div className="item">
           <a href="https://makiwumi.github.io/Employee-Directory/">
           <img src="/static/emppre.png"  />
-          </a>
           <h2>Employee Directory</h2>
-          <p>An app that displays a list of employees by name, phone number and email.</p>
-        </div>
-        <div className="item">
-          <a href="#">
-          <img src="/static/comingpre.png"  />
           </a>
-          <h2>Curbside</h2>
-          <p>An app that makes moving and letting go easier by leaving the things you will like to get rid on the curb while others call dibs and pick it up. Created using MERN stack</p>
-        </div>
-        <div className="item">
-          <a href="#">
-          <img src="/static/comingpre.png"  />
-          </a>
-          <h2>Coming Soon</h2>
-          <p></p>
+          <p id="p1">An app that displays a list of employees by name, phone number and email.</p>
+          <p id="p2">Tools: React.js, Node.js, Express, RandomUser API</p>
         </div>
       </div>
     </div>
